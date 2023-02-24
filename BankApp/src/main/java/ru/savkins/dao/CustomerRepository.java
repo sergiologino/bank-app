632a714d186a3cce11a712386f230f61a6ec34ed
sergiologino <savkin.sergey@yandex.ru>
2023-02-24 19:53:12 +0300

    working project with Postgres localhost

--- BankApp/src/main/java/ru/savkins/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.savkins.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ru.savkins.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-
-}
--- BankApp/src/main/java/ru/savkins/dao/LoggerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.savkins.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ru.savkins.model.Logger;
-
-public interface LoggerRepository extends CrudRepository<Logger, Integer> {
-
-}
