8c3eb90525c1cbedc299dd8e56bca48732398229
sergiologino <savkin.sergey@yandex.ru>
2023-03-14 19:32:59 +0300

    Local deploy, cloud settings commented in application.properties

--- src/main/java/ru/savkins/controller/CustomerController.java
+++ src/main/java/ru/savkins/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package ru.savkins.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +15,13 @@ public class CustomerController {
 	@Autowired
 	private AccountController accountController;
 
+	@Operation(summary = "Создать клиента",description = "Создание клиента")
 	@PostMapping("/customer")
 	public void createCustomer(@RequestBody Customer customer) {
 		customerService.createCustomer(customer);
 		accountController.createAccount(customer.getAcctID(), 0, "Active");
 	}
-
+	@Operation(summary = "Получить инфо по клиенту",description = "Получить инфо по клиенту")
 	@GetMapping("/customer/{acctID}")
 	public Customer getCustomerInfo(@PathVariable int acctID) {
 		return customerService.getCustomerInfo(acctID);
